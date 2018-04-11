90d71a21aebe6be4e912b7154b722ddfdf5e03cd
Projojoboy <22048@ma-web.nl>
2018-04-11 12:48:41 +0200

    Added player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] private float speed;
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Movement();
+	}
+
+    void Movement()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        rb.AddForce(new Vector3(h * speed * Time.fixedDeltaTime * 100, 0, v * speed * Time.fixedDeltaTime * 100));
+    }
+}
